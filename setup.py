1701c8ba98719ff5315a5a2d9e300dd16721da1b
Claudio Satriano <satriano@gmail.com>
2014-05-11 17:23:49 +0200

    Move all C libs to "c_libs" dir.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('lib_rec_filter',
-                    sources = ['tatka_modules/lib_rec_filter.c'])
+                    sources = ['tatka_modules/c_libs/lib_rec_filter.c'])
 module2 = Extension('lib_rec_rms',
-                    sources = ['tatka_modules/lib_rec_rms.c'])
+                    sources = ['tatka_modules/c_libs/lib_rec_rms.c'])
 module3 = Extension('lib_rec_kurtosis',
-                    sources = ['tatka_modules/lib_rec_kurtosis.c'])
+                    sources = ['tatka_modules/c_libs/lib_rec_kurtosis.c'])
 module4 = Extension('lib_rec_cc',
-                    sources = ['tatka_modules/lib_rec_cc.c'])
+                    sources = ['tatka_modules/c_libs/lib_rec_cc.c'])
 module5 = Extension('lib_map_project',
-                    sources = ['tatka_modules/map_project/util.c',
-                               'tatka_modules/map_project/map_project.c',
-                               'tatka_modules/map_project/coord_convert.c'])
+                    sources = ['tatka_modules/c_libs/map_project/util.c',
+                               'tatka_modules/c_libs/map_project/map_project.c',
+                               'tatka_modules/c_libs/map_project/coord_convert.c'])
 module6 = Extension('lib_rosenberger',
                     sources = ['tatka_modules/c_libs/rosenberger/IA_Kdiag.c',
                                'tatka_modules/c_libs/rosenberger/IA_Err.c',
