9351c0050d7f4ca039da20180c377a4d8bec30bb
Claudio Satriano <satriano@gmail.com>
2014-05-12 17:09:35 +0200

    Small code style fixes in C functions.

--- tatka_modules/c_libs/lib_rec_rms.c
+++ tatka_modules/c_libs/lib_rec_rms.c
@@ -7,7 +7,7 @@ void _recursive_rms(const double *signal, double *rms_signal, int npts, float C_
     double mean_sq = 0;
 
     for (i=0; i<npts; i++) {
-        mean_sq = sqrt(C_WIN*pow(signal[i],2.)+(1-C_WIN)*pow(mean_sq,2.));
+        mean_sq = sqrt(C_WIN * pow(signal[i], 2.) + (1 - C_WIN) * pow(mean_sq, 2.));
         rms_signal[i] = mean_sq;
     }
 }
