0f3f4f5fa21322c49e410bab970bb9f837998cd5
Claudio Satriano <satriano@gmail.com>
2014-07-25 13:11:15 +0200

    backtrack2eventdata: fix coding style
    Fix python coding style and remove extra whitespaces.

--- backtrack2eventdata.py
+++ backtrack2eventdata.py
@@ -3,7 +3,7 @@
 import sys
 import os
 from tatka_modules.bp_types import Trigger, Pick
-from obspy import read,UTCDateTime
+from obspy import read
 from obspy.core import AttribDict
 
 """
@@ -13,14 +13,14 @@ to run: ./backtrack2eventdata.py xxx_OUT2_grouped.dat (station.dat --> optional)
 """
 
 main_folder = 'Eventdata'
-basepath_data = '/mnt/data/Tremor_Shikoku/Tremor_data/120531/19/'
+basepath_data = 'example_data'
 out_data_format = "SAC"
 
 # data length to cut before and after the P-wave arrival (in seconds)
 pre_P = 20.0
 post_P = 150.0
 ##
-if len(sys.argv) ==1:
+if len(sys.argv) == 1:
     print "this_code  <triggered_location xxx_OUT2_grouped.dat>"
     sys.exit(1)
 else:
@@ -30,14 +30,14 @@ if len(sys.argv) > 2:
     station_file = sys.argv[2]
 if len(sys.argv) == 2:
     station_file = None
-    
+
 ##--reading station information
 coord_sta = {}
 if station_file:
-    for line in open(station_file,'r'):
-        data = [ x for x in line.split() ]
-        coord_sta[data[0]] = (float(data[2]),float(data[3]),float(data[4]))
-        
+    for line in open(station_file, 'r'):
+        data = line.split()
+        coord_sta[data[0]] = map(float, data[2:5])
+
 ## -----reading output file saving parameters as trigger type--------------------------------------
 triggers = []
 for line in open(infile, 'r'):
@@ -52,8 +52,8 @@ for line in open(infile, 'r'):
             triggers[-1].add_pick(pick)
         except ValueError:
             continue
-        
-##---creating output directories if do not exist--------------------------------------------------
+
+##---creating output directories if they do not exist----------------------------------------------
 if not os.path.exists(main_folder):
     os.mkdir(main_folder)
 for trigger in triggers:
@@ -63,7 +63,7 @@ for trigger in triggers:
 
 ##---outputting event files and data--------------------------------------------------------------
 for trigger in triggers:
-    
+
 ##-------writting enevtid.dat file ---------------------------------------------------------------
     event_dat_base = '.'.join((trigger.eventid,'dat'))
     event_dat = os.path.join(out_event_folder,event_dat_base)
@@ -71,7 +71,7 @@ for trigger in triggers:
         f.write(str(trigger) + '\n')
         for pick in trigger.picks:
             f.write(str(pick) + '\n')
-            
+
 ##-------writting enevtid_nll.dat file -----------------------------------------------------------
     event_dat_base2 = '.'.join((trigger.eventid+'_nll','dat'))
     event_dat2 = os.path.join(out_event_folder,event_dat_base2)
@@ -87,15 +87,15 @@ for trigger in triggers:
             f.write('%04d ' % msec)
             f.write('GAU  5.00e-02  0.00e+00  0.00e+00  0.00e+00')
             f.write('\n')
-    
-## reading data, cutting events and saving data in specified format-------------------------------    
-    for pick in trigger.picks:
-        read_starttime = trigger.origin_time+pick.theor_time - pre_P 
-        read_endtime = trigger.origin_time+pick.theor_time + post_P
-        filename = os.path.join(basepath_data, '*'+pick.station+'*')
 
-        st = read(filename,starttime = read_starttime,
-                  endtime = read_endtime)
+## reading data, cutting events and saving data in specified format-------------------------------
+    for pick in trigger.picks:
+        read_starttime = trigger.origin_time + pick.theor_time - pre_P
+        read_endtime = trigger.origin_time + pick.theor_time + post_P
+        filename = os.path.join(basepath_data, '*' + pick.station + '*')
+
+        st = read(filename, starttime=read_starttime,
+                  endtime=read_endtime)
 
         for tr in st:
             file_out_base = '.'.join((trigger.eventid,
@@ -104,13 +104,13 @@ for trigger in triggers:
                                      tr.stats.network,
                                      tr.stats.location,
                                       out_data_format))
-            file_out_data = os.path.join(out_event_folder,file_out_base)
-            if out_data_format =='SAC':
+            file_out_data = os.path.join(out_event_folder, file_out_base)
+            if out_data_format == 'SAC':
                 tr.stats.sac = AttribDict()
                 tr.stats.sac.kevnm = str(trigger.eventid)
                 if station_file:
                     tr.stats.sac.stla = coord_sta[tr.stats.station][0]
                     tr.stats.sac.stlo = coord_sta[tr.stats.station][1]
                     tr.stats.sac.stel = coord_sta[tr.stats.station][2]
-                    
-            tr.write(file_out_data,format=out_data_format)
+
+            tr.write(file_out_data, format=out_data_format)
