fe5de732c8ddfe1c55690d2c5040a720a11a7cbd
NPoiata <poiata@ipgp.fr>
2014-09-30 17:28:29 +0200

    Fixed bug in recursive filtering module (lib_rec_filter.c)

--- tatka_modules/c_libs/lib_rec_filter.c
+++ tatka_modules/c_libs/lib_rec_filter.c
@@ -10,9 +10,11 @@ void _recursive_filter(const double *signal, double *filt_signal, int npts, floa
     double filterL1 = 0;
     double filterL2 = 0;
 
+    filt_signal[0] = signal[0];
+
     for (i=0; i<npts; i++) {
         filterH1_0 = filterH1;
-        filterH1 = C_HP * (filterH1 + signal[i]);
+        filterH1 = C_HP * (filterH1 + signal[i] - signal[i-1]);
         filterH2 = C_HP * (filterH2 + filterH1 - filterH1_0);
         filterL1 = filterL1 + C_LP * (filterH2 - filterL1);
         filterL2 = filterL2 + C_LP * (filterL1 - filterL2);
