533a1df6bb09e3bd7a93736ff72b082f735331b0
NPoiata <poiata@ipgp.fr>
2014-10-16 19:01:50 +0100

    removed depth from the groupping criteria

--- tatka_modules/mod_groupe_trigs.py
+++ tatka_modules/mod_groupe_trigs.py
@@ -2,9 +2,11 @@ import numpy as np
 from obspy.signal.util import utlGeoKm
 
 def dist_to_evt(trg0,trg):
+##    dist_to_evt0 = np.sqrt(np.power((trg0.x-trg.x),2)+\
+##                   np.power((trg0.y-trg.y),2)+\
+##                   np.power((trg0.z-trg.z),2))
     dist_to_evt0 = np.sqrt(np.power((trg0.x-trg.x),2)+\
-                   np.power((trg0.y-trg.y),2)+\
-                   np.power((trg0.z-trg.z),2))
+                   np.power((trg0.y-trg.y),2))
     time_shift_evt0 = abs(trg0.origin_time-trg.origin_time)
     return dist_to_evt0,time_shift_evt0
 
