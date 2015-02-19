Fix overlapping window calculation.
Next overlapping window should start at (time_lag - overlap) and not at
(overlap).

Example (time_lag=20, overlap=5):
  [0-20], [15-35], [30-50], [45-65], ...

Before it was:
  [0-20], [5-25], [10-30], [15-35], ...

(which is actually an overlap of 15!)

This code also allows now to specify an overlap of 0, i.e. no overlap.

BackProj.py
@@ -34,7 +34,7 @@ def main():
     #---Reading data---------------------------------------------------------
     st = read_traces(config)
     #------------------------------------------------------------------------
-    t_bb = np.arange(config.start_t, config.end_t, config.t_overlap)
+    t_bb = np.arange(config.start_t, config.end_t, config.time_lag - config.t_overlap)
     # selecting the time windows that do not exceed the length of the data---
     t_ee = t_bb + config.time_lag
     data_length = st[0].stats.endtime - st[0].stats.starttime