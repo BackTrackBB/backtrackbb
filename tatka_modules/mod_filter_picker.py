BackTrackBB/backtrackbb

commit 2c524e21e550eac7ab47d4045f995b2ef6d70393

HOS C module modified according to equation (1) in paper, corresponding modification in python modules are included

tatka_modules/mod_filter_picker.py
@@ -34,9 +34,8 @@ def make_LogFq(f_min, f_max, delta, nfreq):
 
 def MBfilter_CF(st, frequencies, var_w=True,
                 CF_type='envelope', CF_decay_win=1.0,
-                order1=4, order2=2, power2=2,
-                rosenberger_decay_win=1.0, wave_type='P',
-                hos_sigma=None,
+                order=4,rosenberger_decay_win=1.0,
+                wave_type='P',hos_sigma=None,
                 full_output=False):
     """
     Performs MBfiltering using 2HP+2LP recursive filter
@@ -54,7 +53,7 @@ def MBfilter_CF(st, frequencies, var_w=True,
     b = CF_decay_nsmps * delta/Tn[int(Nb/2 + 1)]
 
     if hos_sigma is None:
-        hos_sigma = 0.001
+        hos_sigma = -1.
 
     # Less than 3 components
     if len(st) < 3:
@@ -83,7 +82,7 @@ def MBfilter_CF(st, frequencies, var_w=True,
 
             if CF_type == 'kurtosis':
                 CF1[n] = recursive_hos(YN1[n], 1./CF_decay_nsmps_mb,
-                                      hos_sigma, order1, order2, power2)
+                                      hos_sigma, order)
 
     # More than 3 components
     else:
@@ -149,16 +148,16 @@ def MBfilter_CF(st, frequencies, var_w=True,
             if CF_type == 'kurtosis':
                 if wave_type == 'P':
                     CF1[n] = recursive_hos(filteredDataP[n], 1./CF_decay_nsmps_mb,
-                                          hos_sigma, order1, order2, power2)
+                                          hos_sigma, order)
                     if full_output:
                         CF2[n] = recursive_hos(filteredDataS[n], 1./CF_decay_nsmps_mb,
-                                          hos_sigma, order1, order2, power2)
+                                          hos_sigma, order)
                 else:
                     CF1[n] = recursive_hos(filteredDataS[n], 1./CF_decay_nsmps_mb,
                                           hos_sigma, order1, order2, power2)
                     if full_output:
                         CF2[n] = recursive_hos(filteredDataP[n], 1./CF_decay_nsmps_mb,
-                                          hos_sigma, order1, order2, power2)
+                                          hos_sigma, order)
 
     if full_output:
         return YN1, CF1, CF2, Tn, Nb, filteredDataP, filteredDataS

tatka_modules/c_libs/lib_rec_hos.c
@@ -11,7 +11,7 @@
 #include <math.h>
 
 void _recursive_hos(const double *signal, double *hos_signal, int npts,
-        float sigma_min, float C_WIN, int order1, int order2, float power2)
+        float sigma_min, float C_WIN, int order)
 {
     int i;
     int n_win;
@@ -19,18 +19,36 @@ void _recursive_hos(const double *signal, double *hos_signal, int npts,
     double var_temp = 1.0;
     double var = 1;
     double hos = 0;
+    double power = 2;
 
-    for (i=0; i<npts; i++) {
-        mean = C_WIN * signal[i] + (1 - C_WIN) * mean;
+    power = order/2;
+    n_win = (int) 1/C_WIN;
 
-        var_temp = C_WIN * pow((signal[i] - mean),order2) + (1 - C_WIN) * var;
-        if (var_temp > sigma_min) {
-            var = var_temp;
-        } else {
-            var = sigma_min;
-        }
-
-        hos = C_WIN * (pow((signal[i]-mean), order1) / pow((var), power2)) + (1 - C_WIN) * hos;
-        hos_signal[i] = hos;
+    for (i=0; i<n_win; i++) {
+		mean = C_WIN * signal[i] + (1 - C_WIN) * mean;
+		var = C_WIN * pow((signal[i] - mean),2.0) + (1 - C_WIN) * var;
+    }
+    
+    if (sigma_min < 0){
+    	for (i=0; i<npts; i++) {
+        	mean = C_WIN * signal[i] + (1 - C_WIN) * mean;
+        	var = C_WIN * pow((signal[i] - mean),2.0) + (1 - C_WIN) * var;
+        	hos = C_WIN * (pow((signal[i]-mean), order) / pow((var), power)) + (1 - C_WIN) * hos;
+        	hos_signal[i] = hos;
+	}
+    } else {
+    	for (i=0; i<npts; i++) {
+        	mean = C_WIN * signal[i] + (1 - C_WIN) * mean;
+        	var_temp = C_WIN * pow((signal[i] - mean),2.0) + (1 - C_WIN) * var;
+        	if (var_temp > sigma_min) {
+            	  var = var_temp;
+        	} else {
+            	  var = sigma_min;
+        	}
+        	hos = C_WIN * (pow((signal[i]-mean), order) / pow((var), power)) + (1 - C_WIN) * hos;
+        	hos_signal[i] = hos;
+    	}   
     }
 }
+
+

tatka_modules/rec_hos.py
@@ -21,13 +21,11 @@ lib_rec_hos._recursive_hos.argtypes = [
         ctypes.c_float,
         ctypes.c_float,
         ctypes.c_int,
-        ctypes.c_int,
-        ctypes.c_float
         ]
 lib_rec_hos._recursive_hos.restype = ctypes.c_void_p
 
 
-def recursive_hos(signal, C_WIN, sigma_min, order1, order2, power2):
+def recursive_hos(signal, C_WIN, sigma_min, order):
     signal = np.array(signal, dtype=np.float64)
     """
         Recursive computation of higher-horder statistics.
@@ -40,7 +38,7 @@ def recursive_hos(signal, C_WIN, sigma_min, order1, order2, power2):
 
     hos_signal = np.zeros(len(signal))
     lib_rec_hos._recursive_hos(
-            signal, hos_signal, signal.size, sigma_min, C_WIN, order1, order2, power2)
+            signal, hos_signal, signal.size, sigma_min, C_WIN, order)
     return hos_signal
 
 
@@ -48,6 +46,6 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     signal = np.ones(60000)
     signal[30000]+=1
-    kurt_signal = recursive_hos(signal, 0.5, 0.001, 4, 2, 2)
+    kurt_signal = recursive_hos(signal, 0.5, 0.001, 4)
     plt.plot(kurt_signal)
     plt.show()

commit c1b414a40aa4bca08fa61cfe82aa247db6208eda

Fixes in mod_filter_picker for 3-c analysis.
Fix two inconsistencies introduced with 2c524e2 and 9da5323.

tatka_modules/mod_filter_picker.py
@@ -77,7 +77,7 @@ def MBfilter_CF(st, frequencies, var_w=True,
             YN1[n] = recursive_filter(y, CN_HP[n], CN_LP[n], rmem)
 
             if var_w and CF_type == 'envelope':
-                CF_decay_nsmps_mb = (Tn[n]/delta)*CF_decay_nsmps
+                CF_decay_nsmps_mb = (Tn[n]/delta) * CF_decay_nsmps
             else:
                 CF_decay_nsmps_mb = CF_decay_nsmps
 
@@ -131,8 +131,8 @@ def MBfilter_CF(st, frequencies, var_w=True,
             filteredDataS[n] = np.sqrt(np.power(filt_dataS[1,:], 2) +
                                        np.power(filt_dataS[2,:], 2))
 
-            if var_w:
-                CF_decay_nsmps_mb = b * Tn[n]/delta
+            if var_w and CF_type == 'envelope':
+                CF_decay_nsmps_mb = (Tn[n]/delta) * CF_decay_nsmps
             else:
                 CF_decay_nsmps_mb = CF_decay_nsmps
 
@@ -155,7 +155,7 @@ def MBfilter_CF(st, frequencies, var_w=True,
                                           hos_sigma, order)
                 else:
                     CF1[n] = recursive_hos(filteredDataS[n], 1./CF_decay_nsmps_mb,
-                                          hos_sigma, order1, order2, power2)
+                                          hos_sigma, order)
                     if full_output:
                         CF2[n] = recursive_hos(filteredDataP[n], 1./CF_decay_nsmps_mb,
                                           hos_sigma, order)