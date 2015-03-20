bc1b5679d0dd1f531f0958a5ea9badacf6133c1f
Claudio Satriano <satriano@gmail.com>
2015-03-20 17:31:59 +0100

    Rename group_triggers config parameters.
    After all they are a minimum allowed distance and a minimum allowed time
    difference, right?

--- tatka_modules/mod_group_trigs.py
+++ tatka_modules/mod_group_trigs.py
@@ -2,8 +2,8 @@ import math
 
 def trig_dist(trg1, trg2):
     dist = math.sqrt((trg1.x - trg2.x)**2 + (trg1.y - trg2.y)**2)
-    time_shift = abs(trg1.origin_time - trg2.origin_time)
-    return dist, time_shift
+    time_diff = abs(trg1.origin_time - trg2.origin_time)
+    return dist, time_diff
 
 def group_triggers(config, triggers):
     sorted_trg = sorted(triggers, key=lambda x: x.max_grid,
@@ -11,9 +11,9 @@ def group_triggers(config, triggers):
     n = 0
     while True:
         for trg in sorted_trg[n+1:]:
-            dist, time_shift = trig_dist(sorted_trg[n], trg)
-            if (dist <= config.max_dist and
-                time_shift <= config.max_time_shift):
+            dist, time_diff = trig_dist(sorted_trg[n], trg)
+            if (dist <= config.group_min_dist and
+                time_diff <= config.group_min_time_diff):
                 sorted_trg.remove(trg)
         n += 1
         if n == len(sorted_trg):
