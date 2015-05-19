8562e100055f26fb3562c478b315865fc157addc
Claudio Satriano <satriano@gmail.com>
2015-05-19 18:14:47 +0200

    Allow for any prefix in time grid name.
    Not just 'layer'.

--- tatka_modules/read_grids.py
+++ tatka_modules/read_grids.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import os
+from glob import glob
 from collections import defaultdict
 from NLLGrid import NLLGrid
 
@@ -9,8 +10,9 @@ def read_grids(config):
 
     for grid_type in config.grid_type:
         for station in config.stations:
-            grid_bname = '.'.join(('layer', grid_type, station, 'time'))
+            grid_bname = '.'.join(('*', grid_type, station, 'time.hdr'))
             grid_bname = os.path.join(config.grid_dir, grid_bname)
+            grid_bname = glob(grid_bname)[0]
             grid = NLLGrid(grid_bname)
             coord_sta[station] = (grid.sta_x, grid.sta_y)
             GRD_sta[station][grid_type] = grid
