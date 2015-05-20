612e2fb79f329fd1e7bb3b07094abb445ab09b25
Claudio Satriano <satriano@gmail.com>
2015-05-20 14:54:33 +0200

    map_project: add 'SIMPLE' transform.
    Fixes a bug in coord_convert.c where a few constants where not
    initialized. Needs recompiling!

--- tatka_modules/map_project.py
+++ tatka_modules/map_project.py
@@ -25,6 +25,7 @@ lib_map_project.rect2latlon.argtypes = [
         ctypes.POINTER(ctypes.c_double),
         ]
 
+
 def get_transform(trans_type, orig_lat, orig_lon,
                   first_std_paral=None,
                   second_std_paral=None,
@@ -35,8 +36,12 @@ def get_transform(trans_type, orig_lat, orig_lon,
         s = 'LAMBERT %s %s %s %s %s %s' %\
                 (ellipsoid, orig_lat, orig_lon,
                  first_std_paral, second_std_paral, map_rot)
+    if trans_type == 'SIMPLE':
+        s = 'SIMPLE %s %s %s' %\
+                (orig_lat, orig_lon, map_rot)
 
     lib_map_project.get_transform(0, s)
+
 
 def latlon2rect(lat, lon):
     x = ctypes.c_double()
@@ -44,11 +49,13 @@ def latlon2rect(lat, lon):
     lib_map_project.latlon2rect(0, lat, lon, ctypes.byref(x), ctypes.byref(y))
     return x.value, y.value
 
+
 def rect2latlon(x, y):
     lat = ctypes.c_double()
     lon = ctypes.c_double()
     lib_map_project.rect2latlon(0, x, y, ctypes.byref(lat), ctypes.byref(lon))
     return lat.value, lon.value
+
 
 if __name__ == '__main__':
     lat0 = 51.656300
@@ -59,6 +66,12 @@ if __name__ == '__main__':
     get_transform('LAMBERT', lat0, lon0,
                    std_par1, std_par2,
                    rot, 'Clarke-1880')
+    print latlon2rect(lat0, lon0)
+    x, y = latlon2rect(50.1, 7.2)
+    print rect2latlon(x, y)
+    get_transform('SIMPLE', lat0, lon0,
+                   None, None,
+                   rot, None)
     print latlon2rect(lat0, lon0)
     x, y = latlon2rect(50.1, 7.2)
     print rect2latlon(x, y)
