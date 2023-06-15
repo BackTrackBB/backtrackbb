75758795f6015fe560592ed8c3d01f5b1da9faba
Claudio Satriano <satriano@gmail.com>
2023-06-15 08:22:08 +0200

    Use importlib to determine lib path

--- backtrackbb/lib_names.py
+++ backtrackbb/lib_names.py
@@ -1,15 +1,9 @@
 # -*- coding: utf8 -*-
 import os
-import sysconfig
+import importlib.machinery
 
 
 def get_lib_path(lib):
-    suffix = sysconfig.get_config_var('EXT_SUFFIX')
-    if not suffix:
-        suffix = sysconfig.get_config_var('SO')
-    if os.name == 'nt':
-        py_version_nodot = sysconfig.get_config_var('py_version_nodot')
-        platform = sysconfig.get_platform().replace('-', '_')
-        suffix = '.cp{}-{}{}'.format(py_version_nodot, platform, suffix)
+    suffix = importlib.machinery.EXTENSION_SUFFIXES[0]
     libname = lib + suffix
     return os.path.join(os.path.dirname(__file__), 'lib', libname)
